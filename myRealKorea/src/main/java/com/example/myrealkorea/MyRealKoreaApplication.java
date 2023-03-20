55a2994898c885ade89f64793ee16d497f27fd44
ys9890lggdd@naver.com <ys9890lggdd@naver.com>
2023-03-20 14:07:02 +0900

    ..
    Signed-off-by: ys9890lggdd@naver.com <ys9890lggdd@naver.com>

---
+++ myRealKorea/src/main/java/com/example/myrealkorea/MyRealKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrealkorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRealKoreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRealKoreaApplication.class, args);
+    }
+
+}
